4667ee24c8d99a41501c50a793d606db1ac22825
Guantum <guanqun.real@gmail.com>
2025-01-16 16:46:54 +0800

    :wrench: modified the top-right toolbar

--- PuppyFlow/app/components/upbar/Upbar.tsx
+++ PuppyFlow/app/components/upbar/Upbar.tsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import AddNodeButton from '../workflow/buttonControllers/AddNodeButton'
 import TopRightToolBar from './topRightToolBar/TopRightToolBar'
-import TopRightSaveHistoryButton from './TopRightSaveHistoryButton'
+import TopRightSaveHistoryButton from './topRightToolBar/SaveButton'
 function Upbar() {
   return (
     <div className='w-full h-[44px] gap-[8px] absolute top-[32px] z-[10000] pl-[32px] pr-[32px] flex flex-row justify-between items-center flex-wrap pointer-events-none'>
         <AddNodeButton />
         <div className='flex flex-row items-center justify-center gap-[20px]'>
-          <TopRightSaveHistoryButton />
           <TopRightToolBar />
         </div>
     </div>
