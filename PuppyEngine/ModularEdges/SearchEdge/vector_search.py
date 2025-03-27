6a9d2d0b0a4f956e890e7e5fc1e325f9194fb216
ErerGB <zjlpaul@gmail.com>
2025-03-27 19:16:33 +0800

    :bug: Add set_name field to VectorRetrievalStrategy

--- PuppyEngine/ModularEdges/SearchEdge/vector_search.py
+++ PuppyEngine/ModularEdges/SearchEdge/vector_search.py
@@ -66,6 +66,7 @@ class VectorRetrievalStrategy(BaseRetriever):
                 "query": self.query,
                 "model": collection_configs[0].get("model", "text-embedding-ada-002"),
                 "user_id": collection_configs[0].get("user_id", ""),
+                "set_name": collection_configs[0].get("set_name", ""),
                 "vdb_type": collection_configs[0].get("db_type", "pgvector"),
                 "top_k": self.top_k,
                 "threshold": self.threshold,
