f9de12c5eeb8528d22ef06434f9788721b48db9f
Liangyu Chen <zcablc2@ucl.ac.uk>
2025-04-01 12:04:31 +0100

    :wrench: fixed LLM issue

--- PuppyEngine/Server/WorkFlow.py
+++ PuppyEngine/Server/WorkFlow.py
@@ -646,7 +646,7 @@ if __name__ == "__main__":
 
     test_kit = "TestKit/"
     for file_name in os.listdir(test_kit):
-        if file_name != "test_google_search.json":
+        if file_name != "test_new_llm.json":
             continue
 
         file_path = os.path.join(test_kit, file_name)
