7ba0cb2cfa0aeaa6b05fa14bf42a3b4e9266e2bb
Liangyu Chen <zcablc2@ucl.ac.uk>
2025-05-06 16:30:19 +0100

    :wrench: temp llm fix

--- PuppyEngine/Server/WorkFlow.py
+++ PuppyEngine/Server/WorkFlow.py
@@ -653,7 +653,7 @@ if __name__ == "__main__":
 
     test_kit = "TestKit/"
     for file_name in os.listdir(test_kit):
-        if file_name != "test_perplexity2.json":
+        if file_name != "new_llm_bug.json":
             continue
 
         file_path = os.path.join(test_kit, file_name)
