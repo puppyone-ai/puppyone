b5582a09117d616aaeedb08b709b215b0c585e89
Guantum <guanqun.real@gmail.com>
2025-06-02 18:22:40 +0800

    :wrench: modified the edge node hover logic

--- PuppyFlow/app/components/workflow/edgesNode/edgeNodesNew/Generate.tsx
+++ PuppyFlow/app/components/workflow/edgesNode/edgeNodesNew/Generate.tsx
@@ -253,7 +253,7 @@ function Generate({ data, isConnectable, id }: GenerateNodeProps) {
         <div className='p-[3px] w-[80px] h-[48px]'>
             <button 
                 onClick={onClickButton}
-                className={`w-full h-full flex-shrink-0 rounded-[8px] border-[2px] border-[#CDCDCD] text-[#CDCDCD] bg-[#181818] hover:border-main-orange hover:text-main-orange flex items-center justify-center font-plus-jakarta-sans text-[10px] font-[700] ${isOnConnect && isTargetHandleTouched || activatedEdge === id ? "border-main-orange hover:border-main-orange hover:text-main-orange text-main-orange" : "border-[#CDCDCD] text-[#CDCDCD]"} group ${isOnGeneratingNewNode ? "pointer-events-none" : ""}`}
+                className={`w-full h-full flex-shrink-0 rounded-[8px] border-[2px] border-[#CDCDCD] text-[#CDCDCD] bg-[#181818] hover:border-main-orange hover:text-main-orange flex items-center justify-center font-plus-jakarta-sans text-[10px] font-[700]`}
             >
                 Generate
                 <Handle id={`${id}-a`} className='edgeSrcHandle handle-with-icon handle-top' type='source' position={Position.Top} />
