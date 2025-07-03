9b418a4a970b7b0e278778aa231a1465affd7e55
J Z <98926814+ERerGB@users.noreply.github.com>
2025-07-03 19:22:06 +0800

    Revert "Temp/enhance request"

--- PuppyFlow/utils/auth.ts
+++ PuppyFlow/utils/auth.ts
@@ -1,6 +1,6 @@
 import { SYSTEM_URLS } from '@/config/urls'
 
-export async function verifyToken() {
+export async function verifyToken(token: string) {
   try {
     const authServerUrl = SYSTEM_URLS.USER_SYSTEM.BACKEND
     const verifyPath = '/protected'
@@ -8,9 +8,9 @@ export async function verifyToken() {
     const response = await fetch(`${authServerUrl}${verifyPath}`, {
       method: 'GET',
       headers: {
+        'Authorization': `Bearer ${token}`,
         'Content-Type': 'application/json',
-      },
-      credentials: 'include',
+      }
     })
 
     return {
