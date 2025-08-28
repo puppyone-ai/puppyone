dce62ace275d51fa060a6ed7dafc2adee1e40d83
Guantum <guanqun.real@gmail.com>
2025-08-28 17:55:59 +0800

    :wrench: modified the top left/right bar

--- PuppyFlow/app/components/upbar/Upbar.tsx
+++ PuppyFlow/app/components/upbar/Upbar.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import AddNodeButton from './topLeftToolBar/AddNodeButton';
 import TopRightToolBar from './topRightToolBar/TopRightToolBar';
-import TopRightSaveHistoryButton from './topRightToolBar/SaveButton';
 
 function Upbar() {
   return (
-    <div className='w-full h-[44px] gap-[12px] absolute top-[48px] z-[10000] pl-[48px] pr-[48px] flex flex-row justify-between items-center flex-wrap pointer-events-none'>
+    <div className='w-auto h-[44px] gap-[12px] absolute top-[48px] left-1/2 -translate-x-1/2 z-[10000] flex flex-row justify-center items-center pointer-events-none'>
       <div className='flex flex-row items-center gap-6 pointer-events-auto'>
         <AddNodeButton />
-      </div>
-      <div className='flex flex-row items-center justify-center gap-[20px]'>
         <TopRightToolBar />
       </div>
     </div>
