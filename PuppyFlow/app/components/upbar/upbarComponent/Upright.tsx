58ce06f16978bc82ca0a43d2a2146e815f2b4ae0
Guantum <guanqun.real@gmail.com>
2025-08-31 00:11:54 +0800

    :wrench: new upbar

---
+++ PuppyFlow/app/components/upbar/upbarComponent/Upright.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import React from 'react';
+import { Controls } from '@xyflow/react';
+import SaveButton from './SaveButton';
+
+export default function Upright() {
+  return (
+    <div className='flex items-center gap-2 pointer-events-auto'>
+      <Controls
+        className='react-flow__controls-custom'
+        showZoom={true}
+        showFitView={true}
+        showInteractive={false}
+        orientation='horizontal'
+        style={{ position: 'relative' }}
+      />
+      <SaveButton />
+    </div>
+  );
+}
+
+
